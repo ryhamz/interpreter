0cf4afb55376ae2bf01d483916e170487f3686ae
root <ryhamz@gmail.com>
2021-12-23 02:09:19 -0500

    support for parsing integer literals

--- src/monkey/ast/ast.go
+++ src/monkey/ast/ast.go
@@ -45,6 +45,11 @@ type Program struct {
 	Statements []Statement
 }
 
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
@@ -111,3 +116,7 @@ func (es *ExpressionStatement) String() string {
 	}
 	return ""
 }
+
+func (il *IntegerLiteral) expressionNode()      {}
+func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
+func (il *IntegerLiteral) String() string       { return il.Token.Literal }
